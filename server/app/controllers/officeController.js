ea51973598b5342add977ef84e20e7075d39c52f
chuxmykel <ngwobiachukwudi@gmail.com>
2019-02-19 22:21:09 +0100

    feat(endpoint): Implement Get all offices - work on the get all offices endpoint [Delivers #164030308]

--- server/app/controllers/officeController.js
+++ server/app/controllers/officeController.js
@@ -19,10 +19,23 @@ class OfficeController {
     offices.push(office);
     return res.status(201).send({
       status: 201,
-      data: [{
-        id: office.id,
-        name: office.name,
-      }],
+      data: [
+        offices[offices.length - 1],
+      ],
+    });
+  }
+
+  /**
+  * @method getAllOffices
+  * @description Gets all the offices
+  * @param {object} req - The Request Object
+  * @param {object} res - The Response Object
+  * @returns {object} JSON API Response
+  */
+  getAllOffices(req, res) {
+    return res.status(200).send({
+      status: 201,
+      data: offices,
     });
   }
 }
